88361ee3f766ee7762307e1773ca0e4b67fa7b0e
Leonardo Cottino <leonardo.cottino@hotmail.it>
2026-07-21 19:18:14 +0200

    feat(cart): add CartService

--- src/main/java/com/app/ecommerceapp/repository/CartItemRepository.java
+++ src/main/java/com/app/ecommerceapp/repository/CartItemRepository.java
@@ -5,9 +5,12 @@ import com.app.ecommerceapp.model.Product;
 import com.app.ecommerceapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
     Optional<CartItem> findByUserAndProduct(User user, Product product);
+
+    List<CartItem> findAllByUserId(Long userId);
 }
